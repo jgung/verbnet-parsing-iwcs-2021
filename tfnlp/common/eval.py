496c8cbe31e2dba5c2ebc814dad2b30649dff1f7
James Gung <gungjm@gmail.com>
2019-01-31 09:02:02 -0700

    Handle raw inputs in CoNLL eval

--- tfnlp/common/eval.py
+++ tfnlp/common/eval.py
@@ -40,7 +40,7 @@ def conll_eval(gold_batches, predicted_batches, indices, output_file):
         for line in get_lines():
             output.write(line + '\n')
 
-    result = conll_eval_lines(get_lines()).to_conll_output()
+    result = conll_eval_lines(get_lines(), raw=True).to_conll_output()
     return float(re.split('\s+', re.split('\n', result)[1].strip())[7]), result
 
 
