4a7c22cff76b0aeb9751812525920b2aae178994
James Gung <gungjm@gmail.com>
2019-02-05 14:19:40 -0700

    Use rel/head keys in parser exports

--- tfnlp/model/parser.py
+++ tfnlp/model/parser.py
@@ -106,8 +106,8 @@ class ParserHead(ModelHead):
             self.evaluation_hooks.append(hook)
 
     def _prediction(self):
-        self.export_outputs = {constants.ARC_PROBS: PredictOutput({constants.ARC_PROBS: self.arc_probs}),
-                               constants.REL_PROBS: PredictOutput({constants.REL_PROBS: self.rel_probs})}
+        self.export_outputs = {constants.HEAD_KEY: PredictOutput({constants.HEAD_KEY: self.arc_probs}),
+                               constants.DEPREL_KEY: PredictOutput({constants.DEPREL_KEY: self.rel_probs})}
 
 
 def get_shape(tensor):
