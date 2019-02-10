21d18af11a289f1fc626cb8594e4ba1d7bbd0115
James Gung <gungjm@gmail.com>
2019-02-10 12:53:37 -0700

    Add co-agent/patient marker in mappings, add function to apply argument function tag mappings

--- tfnlp/common/srl_utils.py
+++ tfnlp/common/srl_utils.py
@@ -2,7 +2,7 @@ import glob
 import os
 import re
 from collections import defaultdict
-from typing import Dict
+from typing import Dict, Optional
 from xml.etree import ElementTree
 
 _PREDICATE = 'predicate'
@@ -13,8 +13,11 @@ _ID = 'id'
 _NUMBER = 'n'
 _FT = 'f'
 
+VALID_NUMBERS = {'0', '1', '2', '3', '4', '5', '6', 'A', 'M'}
 
-def get_argument_function_mappings(frames_dir: str) -> Dict[str, Dict[str, str]]:
+
+def get_argument_function_mappings(frames_dir: str,
+                                   add_co_marker: bool = True) -> Dict[str, Dict[str, str]]:
     """
     Return a dictionary from roleset IDs (e.g. 'take.01') to dictionaries mapping numbered arguments to function tags.
 
@@ -25,7 +28,8 @@ def get_argument_function_mappings(frames_dir: str) -> Dict[str, Dict[str, str]]
     'PAG'
 
     :param frames_dir: directory containing PropBank frame XML files.
-    :return: mappings from arguments to function tags, by roleset ID
+    :param add_co_marker: if 'True', add '2' if function is already present, and is secondary, e.g. co-patient -> PPT2
+    :return: mappings from arguments to function tags, split by roleset ID
     """
     mappings = defaultdict(dict)
     for framefile in glob.glob(os.path.join(frames_dir, '*.xml')):
@@ -34,14 +38,25 @@ def get_argument_function_mappings(frames_dir: str) -> Dict[str, Dict[str, str]]
             for roleset in predicate.findall(_ROLESET):
                 rs_mappings = mappings[roleset.get(_ID)]
                 for roles in roleset.findall(_ROLES):
-                    for role in roles.findall(_ROLE):
+                    fts = set()  # sometimes, there will be multiple of the same FT, e.g. two PAG, in which case we add 'PAG2'
+
+                    # roles should be in alphanumeric order, so we properly assign co-patient/co-agent
+                    sorted_roles = sorted(roles.findall(_ROLE), key=lambda r: r.get(_NUMBER).upper())
+                    for role in sorted_roles:
                         number = role.get(_NUMBER).upper()
+                        if number not in VALID_NUMBERS:
+                            raise ValueError('Unexpected number format: %s' % number)
                         ft = role.get(_FT).upper()
+                        if add_co_marker:
+                            if ft in fts:
+                                ft = ft + '2'
+                            else:
+                                fts.add(ft)
                         rs_mappings[number] = ft
     return mappings
 
 
-NUMBER_PATTERN = r'(?:A|ARG)([A\d]|M(?=-))'
+NUMBER_PATTERN = r'(?:ARG|A)([A\d]|M(?=-))'
 
 
 def get_number(role: str) -> str:
@@ -55,9 +70,34 @@ def get_number(role: str) -> str:
     'M'
 
     :param role: role string, e.g. 'ARG4' or 'A4'
-    :return: role number string
+    :return: single-character role number string, e.g. '4'
     """
     numbers = re.findall(NUMBER_PATTERN, role, re.IGNORECASE)
     if not numbers:
         raise ValueError('Unsupported or invalid PropBank role format: %s' % role)
     return numbers[0]
+
+
+def apply_numbered_arg_mappings(roleset_id: str,
+                                role: str,
+                                mappings: Dict[str, Dict[str, str]],
+                                ignore_unmapped: bool = False) -> Optional[str]:
+    """
+    Apply argument mappings for a given roleset and role.
+    :param roleset_id: roleset ID, e.g. 'take.01'
+    :param role: role string, e.g. 'A4'
+    :param mappings: dictionary of mappings from numbered arguments by roleset
+    :param ignore_unmapped: if 'True', return unmodified role string if mapping is not present
+    :return: mapped role, or 'None' if no mapping exists and ignore_unmapped is set to 'False'
+    """
+    roleset_map = mappings[roleset_id]
+    if not roleset_map:
+        raise ValueError('Missing roleset in mappings: %s' % roleset_id)
+
+    role_number = get_number(role)  # e.g. 'A4' -> '4'
+    mapped = roleset_map.get(role_number)  # e.g. '4' -> 'GOL'
+    if not mapped:
+        if ignore_unmapped:
+            return role
+        return None
+    return mapped
