dfbacb7d0086b532209144a65695bbcd99b60c6d
jamesgung <gungjm@gmail.com>
2019-05-18 12:20:35 -0400

    Refactor evaluators

--- tfnlp/cli/evaluators.py
+++ tfnlp/cli/evaluators.py
@@ -1,9 +1,8 @@
 import os
 
 import tensorflow as tf
-from tfnlp.common.bert import BERT_SUBLABEL
-
 from tfnlp.common import constants
+from tfnlp.common.bert import BERT_SUBLABEL
 from tfnlp.common.eval import append_srl_prediction_output, write_props_to_file
 from tfnlp.common.eval import conll_eval, conll_srl_eval, parser_write_and_eval
 from tfnlp.common.utils import binary_np_array_to_unicode
@@ -40,26 +39,30 @@ class Evaluator(object):
         pass
 
 
+class TokenClassifierEvaluator(Evaluator):
+    def __init__(self, target=None, output_path=None, script_path=None):
+        super().__init__(target, output_path, script_path)
+
+    def __call__(self, labeled_instances, results):
+        super().__call__(labeled_instances, results)
+
+
 class TaggerEvaluator(Evaluator):
 
     def __init__(self, target=None, output_path=None, script_path=None):
         super().__init__(target, output_path, script_path)
 
     def __call__(self, labeled_instances, results):
-        tagger_evaluator(labeled_instances, results, self.output_path, self.target.name)
-
-
-def tagger_evaluator(labeled_instances, results, output_path=None, target_key=None):
-    target_key = constants.LABEL_KEY if not target_key else target_key
-    labels = []
-    gold = []
-    indices = []
-    for instance, result in zip(labeled_instances, results):
-        labels.append([label for label in binary_np_array_to_unicode(result[target_key]) if label != BERT_SUBLABEL])
-        gold.append(instance[constants.LABEL_KEY])
-        indices.append(instance[constants.SENTENCE_INDEX])
-    f1, result_str = conll_eval(gold, labels, indices, output_file=output_path)
-    tf.logging.info(result_str)
+        target_key = constants.LABEL_KEY if not self.target.name else self.target.name
+        labels = []
+        gold = []
+        indices = []
+        for instance, result in zip(labeled_instances, results):
+            labels.append([label for label in binary_np_array_to_unicode(result[target_key]) if label != BERT_SUBLABEL])
+            gold.append(instance[constants.LABEL_KEY])
+            indices.append(instance[constants.SENTENCE_INDEX])
+        f1, result_str = conll_eval(gold, labels, indices, output_file=self.output_path)
+        tf.logging.info(result_str)
 
 
 class DepParserEvaluator(Evaluator):
@@ -67,32 +70,28 @@ class DepParserEvaluator(Evaluator):
         super().__init__(target, output_path, script_path)
 
     def __call__(self, labeled_instances, results):
-        dep_evaluator(labeled_instances, results, self.target, self.script_path, self.output_path)
+        arc_probs = []
+        rel_probs = []
+        gold_arcs = []
+        gold_rels = []
 
+        for instance, result in zip(labeled_instances, results):
+            seq_len = 1 + len(instance[constants.WORD_KEY])  # plus 1 for head
+            gold_arcs.append([0] + instance[constants.HEAD_KEY])
+            gold_rels.append(['<ROOT>'] + instance[constants.DEPREL_KEY])
 
-def dep_evaluator(labeled_instances, results, features, script_path, output_path=None):
-    arc_probs = []
-    rel_probs = []
-    gold_arcs = []
-    gold_rels = []
+            arc_probs.append(result[constants.ARC_PROBS][:seq_len, :seq_len])
+            rel_probs.append(result[constants.REL_PROBS])
 
-    for instance, result in zip(labeled_instances, results):
-        seq_len = 1 + len(instance[constants.WORD_KEY])  # plus 1 for head
-        gold_arcs.append([0] + instance[constants.HEAD_KEY])
-        gold_rels.append(['<ROOT>'] + instance[constants.DEPREL_KEY])
-
-        arc_probs.append(result[constants.ARC_PROBS][:seq_len, :seq_len])
-        rel_probs.append(result[constants.REL_PROBS])
-
-    result = parser_write_and_eval(arc_probs=arc_probs,
-                                   rel_probs=rel_probs,
-                                   heads=gold_arcs,
-                                   rels=gold_rels,
-                                   script_path=script_path,
-                                   features=features,
-                                   out_path=output_path,
-                                   gold_path=output_path + '.gold')
-    tf.logging.info('\n%s', result)
+        result = parser_write_and_eval(arc_probs=arc_probs,
+                                       rel_probs=rel_probs,
+                                       heads=gold_arcs,
+                                       rels=gold_rels,
+                                       script_path=self.script_path,
+                                       features=self.target,
+                                       out_path=self.output_path,
+                                       gold_path=self.output_path + '.gold')
+        tf.logging.info('\n%s', result)
 
 
 class SrlEvaluator(Evaluator):
@@ -100,27 +99,23 @@ class SrlEvaluator(Evaluator):
         super().__init__(target, output_path, script_path)
 
     def __call__(self, labeled_instances, results):
-        srl_evaluator(labeled_instances, results, self.output_path, constants.LABEL_KEY)
+        labels = []
+        gold = []
+        markers = []
+        indices = []
+        for instance, result in zip(labeled_instances, results):
+            labels.append([label for label in binary_np_array_to_unicode(result[constants.LABEL_KEY]) if label != BERT_SUBLABEL])
+            gold.append(instance[constants.LABEL_KEY])
+            markers.append(instance[constants.MARKER_KEY])
+            indices.append(instance[constants.SENTENCE_INDEX])
 
+        write_props_to_file(self.output_path + '.gold.conll', gold, markers, indices)
+        write_props_to_file(self.output_path + '.conll', gold, markers, indices)
 
-def srl_evaluator(labeled_instances, results, output_path=None, target_key=None):
-    labels = []
-    gold = []
-    markers = []
-    indices = []
-    for instance, result in zip(labeled_instances, results):
-        labels.append([label for label in binary_np_array_to_unicode(result[target_key]) if label != BERT_SUBLABEL])
-        gold.append(instance[constants.LABEL_KEY])
-        markers.append(instance[constants.MARKER_KEY])
-        indices.append(instance[constants.SENTENCE_INDEX])
+        result = conll_srl_eval(gold, labels, markers, indices)
+        tf.logging.info(result)
 
-    write_props_to_file(output_path + '.gold.conll', gold, markers, indices)
-    write_props_to_file(output_path + '.conll', gold, markers, indices)
+        job_dir = os.path.dirname(self.output_path)
 
-    result = conll_srl_eval(gold, labels, markers, indices)
-    tf.logging.info(result)
-
-    job_dir = os.path.dirname(output_path)
-
-    # append results to summary file
-    append_srl_prediction_output(os.path.basename(output_path), result, job_dir, output_confusions=True)
+        # append results to summary file
+        append_srl_prediction_output(os.path.basename(self.output_path), result, job_dir, output_confusions=True)
