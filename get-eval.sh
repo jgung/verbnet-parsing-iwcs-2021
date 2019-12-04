e831dc0fbc530ec075085c2fd72db8fbf1b8e50b
James Gung <gungjm@gmail.com>
2019-12-04 13:43:43 -0500

    Script to extract evaluation results from gcloud

---
+++ get-eval.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+job="$1"
+jobpath="gs://gunglab/experiments/${job}"
+out="$2"
+outpath=${out}
+
+mkdir -p ${outpath}
+
+gsutil cp ${jobpath}/config.json ${outpath}
+gsutil cp ${jobpath}/eval-summary.tsv ${outpath}
+gsutil cp ${jobpath}/train.log ${outpath}
+gsutil cp ${jobpath}/eval.log ${outpath}
+gsutil cp ${jobpath}/*.eval*.txt ${outpath}
