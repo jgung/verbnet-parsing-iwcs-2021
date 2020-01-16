7dc135ba207b22825693bca032abbb19b05f1438
James Gung <gungjm@gmail.com>
2020-01-16 17:09:05 -0500

    Remove evaluation settings specific to thesis experiments

--- tfnlp/cli/evaluators.py
+++ tfnlp/cli/evaluators.py
@@ -1,12 +1,10 @@
 import subprocess
 import unicodedata
-from collections import defaultdict
 from typing import List
 
 import numpy as np
 import tensorflow as tf
 from sklearn.metrics import classification_report
-from tensorflow.python.framework.errors_impl import NotFoundError
 from tensorflow.python.lib.io import file_io
 
 from tfnlp.common import constants
@@ -15,28 +13,8 @@ from tfnlp.common.config import append_label
 from tfnlp.common.eval import conll_eval, conll_srl_eval, append_prediction_output
 from tfnlp.common.eval import write_props_to_file, get_parse_prediction, \
     to_conll09_line, to_conllx_line, write_parse_result_to_file
-from tfnlp.common.utils import read_json
-
-MONOSEMOUS = "MONOSEMOUS"
-POLYSEMOUS = "POLYSEMOUS"
-
-PROPS = {
-    MONOSEMOUS: False,
-    POLYSEMOUS: False
-}
 
 PUNCT_CAT = {"Pc", "Pd", "Ps", "Pe", "Pi", "Pf", "Po"}
-
-
-def read_senses(file='senses.json'):
-    try:
-        return read_json(file, as_params=False)
-    except NotFoundError:
-        tf.logging.warning("Unable to read senses file: %s" % file)
-        return defaultdict(list)
-
-
-SENSES = read_senses()
 
 
 def get_evaluator(heads, feature_extractor, output_path, script_path):
@@ -357,41 +335,24 @@ class SrlEvaluator(TaggerEvaluator):
     def __init__(self, target=None, output_path=None, script_path=None):
         super().__init__(target, output_path, script_path)
         self.markers = None
-        self.senses = []
 
     def start(self):
         super().start()
         self.markers = []
-        self.senses = []
 
     def accumulate(self, instance, result):
-        lemma = instance.get(constants.PREDICATE_LEMMA)
-        if lemma is not None:
-            if PROPS.get(MONOSEMOUS) and len(SENSES.get(lemma, [])) > 1:
-                return
-            elif PROPS.get(POLYSEMOUS) and len(SENSES.get(lemma, [])) <= 1:
-                return
-
-        sense = instance.get(constants.SENSE_KEY)
-        if sense is not None and isinstance(sense, str):
-            self.senses.append((instance[constants.PREDICATE_INDEX_KEY],
-                                instance[constants.PREDICATE_LEMMA] + '.' + sense))
-        self.markers.append(instance[constants.MARKER_KEY])
         super().accumulate(instance, result)
+        self.markers.append(instance[constants.MARKER_KEY])
 
     def evaluate(self, identifier='.'):
         write_props_to_file(self.output_path + '.gold.txt', self.gold, self.markers, self.indices)
         write_props_to_file(self.output_path + '.txt', self.labels, self.markers, self.indices)
 
-        result = conll_srl_eval(self.gold, self.labels, self.markers, self.indices, self.senses)
+        result = conll_srl_eval(self.gold, self.labels, self.markers, self.indices)
         res = str(result)
         tf.logging.info(res)
         p, r, f1 = result.evaluation.prec_rec_f1()
         if self.output_path is not None:
-            if PROPS.get(MONOSEMOUS):
-                identifier = identifier + ".mono"
-            elif PROPS.get(POLYSEMOUS):
-                identifier = identifier + ".poly"
             line = '%s\t%d\t%f\t%f\t%f\t%f' % (identifier,
                                                result.ntargets,
                                                result.perfect_props(),
